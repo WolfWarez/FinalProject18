257c1954f137c175f40a4e57661f2f90cd938079
WolfWarez <JWheelerIII@yahoo.com>
2018-05-20 20:24:15 -0700

    Attempted Time
    Did not succeed

--- Software/fileWriter.cpp
+++ Software/fileWriter.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <fstream>
-#include <chrono>
+#include <ctime>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
+  time_t rawtime;
+  struct tm*timeinfo;
+  char fileName[25];
+  string date;
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
   ofstream outputFile;
-  outputFile.open("example.csv");
-  outputFile << "1,2,3\r\n";
-  outputFile << "4,5,6\r\n";
+  date = asctime(timeinfo);
+  printf(date);
+  sprintf(fileName, "%s.csv", date);
+  printf(fileName);
+  outputFile.open(fileName);
+  outputFile << "1,2,3\n";
+  outputFile << "4,5,6\n";
   outputFile.close();
 }
+
