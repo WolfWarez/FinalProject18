77516bb3f01dd90136147c2e2d8a9555e4c1a1be
James <JWheelerIII@yahoo.com>
2018-05-22 11:13:46 -0700

    Began Work on Visual Display
    Changes to Shapes and Windows still needed

--- Software/fileWriter.cpp
+++ Software/fileWriter.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <fstream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
@@ -17,21 +18,61 @@ const string currentDateTime() {
     return buf;
 }
 
+sf::CircleShape RCircleMaker(string);
+sf::CircleShape LCircleMaker(string);
 
 int main()
 {
-  time_t rawtime;
-  struct tm*timeinfo;
-  string fileName;
-  string fileExtension = ".csv";
-  ofstream outputFile;
+    //Variables: Time Stuff
+    time_t rawtime;
+    struct tm*timeinfo;
+    //Variables: File Stuff
+    string fileName;
+    string fileExtension = ".csv";
+    ofstream outputFile;
+    string serialInput;
+    //Variables: Visual Stuff
+    sf::CircleShape RCircle;
+    sf::CircleShape LCircle;
+    //End of Variables
 
-  cout << "currentDateTime()=" << currentDateTime() << endl;
-  fileName = currentDateTime()+fileExtension;
-  cout << fileName << endl;
-  outputFile.open(fileName);
-  outputFile << "1,2,3\n";
-  outputFile << "4,5,6\n";
-  outputFile.close();
+    //To Be Added: Serial Input
+    serialInput = "R18950";
+    //Generating and Writing to the File
+    cout << "currentDateTime()=" << currentDateTime() << endl;
+    fileName = currentDateTime()+fileExtension;
+    cout << fileName << endl;
+    outputFile.open(fileName);
+    outputFile << "1,2,3\n";
+    outputFile << "4,5,6\n";
+    outputFile.close();
+    //Visual Representation
+    sf::RenderWindow window(sf::VideoMode(800, 600), "JWFIN Newton's Cradle");
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear();
+        RCircle = RCircleMaker(serialInput);
+        RCircle = LCircleMaker(serialInput);
+        window.draw(RCircle);
+        window.draw(LCircle);
+        window.display();
+    }
 }
 
+sf::CircleShape RCicleMaker(string input)
+{
+
+}
+
+sf::CircleShape LCicleMaker(string input)
+{
+
+}
